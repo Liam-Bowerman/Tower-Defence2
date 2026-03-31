9a04b5596b157e6f9ec0095e53989b439713bb1d
Akemi <liboad0@student.oaisd.org>
2026-03-31 13:11:18 -0400

    Added landscape and basic script for grid position

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -7,6 +7,8 @@ namespace TowerDefense
 {
     public class Grid : MonoBehaviour
     {
+        public static float cellSize = 1.0f;
+        public static Transform gridOrigin;
         private Dictionary<Vector3Int, GameObject> gameObjects = new Dictionary<Vector3Int, GameObject>();
 
         public bool Add(Vector3Int tileCoordinates, GameObject gameObject)
@@ -25,14 +27,21 @@ namespace TowerDefense
             gameObjects.Remove(tileCoordinates);
         }
 
-        public void WorldToGrid()
+        public static Vector3Int WorldToGrid(Vector3 position)
         {
-
+            int x = (int)Mathf.Round(position.x);
+            int y = (int)Mathf.Round(position.y);
+            int z = (int)Mathf.Round(position.z);
+            return new Vector3Int(x, y, z);
         }
 
-        public void GridToWorld()
+        public static Vector3 GridToWorld(Vector3Int tileCoordinates)
         {
-
+            float x = tileCoordinates.x * cellSize;
+            float y = tileCoordinates.y * cellSize;
+            float z = tileCoordinates.z * cellSize;
+            Vector3 localPosition = new Vector3(x, y, z);
+            return gridOrigin.TransformPoint(localPosition);
         }
     }
 }
